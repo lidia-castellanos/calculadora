18fde602185333200ee545cd1f49a951ae2a2f96
Lidia Castellanos Covarrubias <105387060+lidia-castellanos@users.noreply.github.com>
2024-03-14 23:12:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 function cargarBotones() {
-    console.log("aqui");
+
     let numeros = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "+-", "0", "."];
-    let simbolos = ["+", "-", "x", "/", "CE", "C","=", "<-"];
+    let simbolos = ["CE", "C", "+", "-", "x", "/", "=",];
     let gridNumeros = document.getElementById("gridNumeros");
-    let gridSimbolos=document.getElementById("simbolos");
+    let gridSimbolos = document.getElementById("simbolos");
+    let valorInputBox;
 
     for (let i = 0; i < numeros.length; i++) {
 
         let btn = document.createElement("button");
         btn.innerText = numeros[i];
-        btn.className="botones";
-        gridNumeros.appendChild(btn);
+        btn.className = "botones";
 
+        gridNumeros.appendChild(btn);
+        btn.onclick = function mostrarBoton() {
+            let textBox = document.getElementById("textBox");
+            
+            
+
+
+
+        }
     }
     for (let i = 0; i < simbolos.length; i++) {
 
         let btn = document.createElement("button");
-        btn.className="botones";
+        btn.className = "botonesSim";
         btn.innerText = simbolos[i];
         gridSimbolos.appendChild(btn);
 
+
     }
-    
+
+
+
+
+
 
 
 }
